ecf22c0af7443b09b4423df77fe31ab2c340b65e
Daljeet Singh <devdaljeetsingh@gmail.com>
2022-10-26 14:29:30 +0000

    Update genesis files

--- x/mitoblockchaindev/genesis.go
+++ x/mitoblockchaindev/genesis.go
@@ -8,6 +8,13 @@ import (
 
 // InitGenesis initializes the module's state from a provided genesis state.
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
+	// Set all the discountTokenStatus
+	for _, elem := range genState.DiscountTokenStatusList {
+		k.SetDiscountTokenStatus(ctx, elem)
+	}
+
+	// Set discountTokenStatus count
+	k.SetDiscountTokenStatusCount(ctx, genState.DiscountTokenStatusCount)
 	// this line is used by starport scaffolding # genesis/module/init
 	k.SetParams(ctx, genState.Params)
 }
@@ -17,6 +24,8 @@ func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
 	genesis := types.DefaultGenesis()
 	genesis.Params = k.GetParams(ctx)
 
+	genesis.DiscountTokenStatusList = k.GetAllDiscountTokenStatus(ctx)
+	genesis.DiscountTokenStatusCount = k.GetDiscountTokenStatusCount(ctx)
 	// this line is used by starport scaffolding # genesis/module/export
 
 	return genesis
