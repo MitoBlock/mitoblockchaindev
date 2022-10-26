edc7eded67903a3a6377576a88bef4631c4f744f
Daljeet Singh <devdaljeetsingh@gmail.com>
2022-10-26 14:33:09 +0000

    Implement keeper for discount token status

--- x/mitoblockchaindev/keeper/grpc_query_discount_tokens.go
+++ x/mitoblockchaindev/keeper/grpc_query_discount_tokens.go
@@ -4,10 +4,10 @@ import (
 	"context"
 
 	"github.com/cosmos/cosmos-sdk/store/prefix"
-    sdk "github.com/cosmos/cosmos-sdk/types"
-    "github.com/cosmos/cosmos-sdk/types/query"
-    "google.golang.org/grpc/codes"
-    "google.golang.org/grpc/status"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"mitoblockchaindev/x/mitoblockchaindev/types"
 )
@@ -18,34 +18,34 @@ func (k Keeper) DiscountTokens(c context.Context, req *types.QueryDiscountTokens
 	}
 
 	// Define a variable that will store a list of posts
-    var discountTokens []*types.DiscountToken
+	var discountTokens []*types.DiscountToken
 
-    // Get context with the information about the environment
-    ctx := sdk.UnwrapSDKContext(c)
+	// Get context with the information about the environment
+	ctx := sdk.UnwrapSDKContext(c)
 
-    // Get the key-value module store using the store key (in our case store key is "chain")
-    store := ctx.KVStore(k.storeKey)
+	// Get the key-value module store using the store key (in our case store key is "chain")
+	store := ctx.KVStore(k.storeKey)
 
-    // Get the part of the store that keeps discountTokens (using discount token key, which is "DiscountToken-value-")
-    postStore := prefix.NewStore(store, []byte(types.DiscountTokenKey))
+	// Get the part of the store that keeps discountTokens (using discount token key, which is "DiscountToken-value-")
+	postStore := prefix.NewStore(store, []byte(types.DiscountTokenKey))
 
-    // Paginate the discount tokens store based on PageRequest
-    pageRes, err := query.Paginate(postStore, req.Pagination, func(key []byte, value []byte) error {
-        var discountToken types.DiscountToken
-        if err := k.cdc.Unmarshal(value, &discountToken); err != nil {
-            return err
-        }
+	// Paginate the discount tokens store based on PageRequest
+	pageRes, err := query.Paginate(postStore, req.Pagination, func(key []byte, value []byte) error {
+		var discountToken types.DiscountToken
+		if err := k.cdc.Unmarshal(value, &discountToken); err != nil {
+			return err
+		}
 
-        discountTokens = append(discountTokens, &discountToken)
+		discountTokens = append(discountTokens, &discountToken)
 
-        return nil
-    })
+		return nil
+	})
 
-    // Throw an error if pagination failed
-    if err != nil {
-        return nil, status.Error(codes.Internal, err.Error())
-    }
+	// Throw an error if pagination failed
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
 
-    // Return a struct containing a list of discount tokens and pagination info
-    return &types.QueryDiscountTokensResponse{DiscountToken: discountTokens, Pagination: pageRes}, nil
+	// Return a struct containing a list of discount tokens and pagination info
+	return &types.QueryDiscountTokensResponse{DiscountToken: discountTokens, Pagination: pageRes}, nil
 }
