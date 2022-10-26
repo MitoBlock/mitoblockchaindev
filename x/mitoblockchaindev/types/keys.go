1328c57780d407a3de83860a569d729f4731387a
Daljeet Singh <devdaljeetsingh@gmail.com>
2022-10-26 14:31:53 +0000

    Add keys for discount token status

--- x/mitoblockchaindev/types/keys.go
+++ x/mitoblockchaindev/types/keys.go
@@ -21,3 +21,8 @@ const (
 func KeyPrefix(p string) []byte {
 	return []byte(p)
 }
+
+const (
+	DiscountTokenStatusKey      = "DiscountTokenStatus/value/"
+	DiscountTokenStatusCountKey = "DiscountTokenStatus/count/"
+)
