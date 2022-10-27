1a9b83c746e3eb92037658cc1fe657233cef8dbb
Daljeet Singh <devdaljeetsingh@gmail.com>
2022-10-27 01:28:42 +0000

    Add the CLI commands for membership token

--- x/mitoblockchaindev/client/cli/query.go
+++ x/mitoblockchaindev/client/cli/query.go
@@ -31,6 +31,8 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	cmd.AddCommand(CmdShowDiscountTokenStatus())
 	cmd.AddCommand(CmdDiscountTokenStatusQ())
 
+	cmd.AddCommand(CmdMembershipTokens())
+
 	// this line is used by starport scaffolding # 1
 
 	return cmd
